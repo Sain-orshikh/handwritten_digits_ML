34654288e773f2713392bbc9b4f0ce83bfb44405
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2026-02-15 09:36:02 +0800

    added pygame interactive ui

--- digits.py
+++ digits.py
@@ -64,10 +64,72 @@ total = 0
 model.eval()
 
 with torch.no_grad():
-    for images, label in testloader:
+    for images, labels in testloader:
         outputs = model(images)
         _, predictions = torch.max(outputs, 1) #torch max returns the max prob with the index
         total += labels.size(0)
         correct += (predictions == labels).sum().item()
 
 print(f"Accuracy: {100*correct/total:.2f}%")
+
+# pygame
+
+def draw_digit():
+    pygame.init()
+    window_size = 280 #28*28 10x
+    display_height = window_size + 50
+    screen = pygame.display.set_mode((window_size, display_height))
+    pygame.display.set_caption("Draw a digit")
+    clock = pygame.time.Clock()
+    screen.fill((0, 0, 0))
+    drawin = False
+    predictions = None
+
+    font = pygame.font.Font(None, 36)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                drawin = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                drawin = False
+                predictions = predict_digit(screen)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    screen.fill((0, 0, 0))
+                    predictions = None
+            if event.type == pygame.MOUSEMOTION and drawin:
+                pygame.draw.circle(screen, (255, 255, 255), event.pos, 8)
+
+        if predictions is not None:
+                text = font.render(f"Prediction: {predictions}", True, (0, 255, 0))
+                screen.blit(text, (10, window_size + 10))
+            
+        pygame.display.flip()
+        clock.tick(60)
+
+#Processing the drawing
+def process_drawing(screen):
+    surface = pygame.surfarray.array3d(screen)
+    gray = np.dot(surface[..., :3], [0.2989, 0.587, 0.114])
+    gray = np.transpose(gray, (1, 0))
+    gray = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
+    gray = gray.astype(np.float32) / 255.0
+    gray = (gray -0.5) / 0.5
+    tensor = torch.tensor(gray, dtype = torch.float32).unsqueeze(0).unsqueeze(0)
+    return tensor
+
+def predict_digit(screen):
+    image = process_drawing(screen)
+    if image is None:
+        return None
+    
+    model.eval()
+    with torch.no_grad():
+        output=model(image)
+        _, prediction = torch.max(output, 1)
+    return prediction.item()
+
+draw_digit()
