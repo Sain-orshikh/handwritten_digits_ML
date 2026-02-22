993d4c2bce61cff9ee752264fd32d91ae5a2e407
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2026-02-22 20:56:40 +0800

    replaced ANN with CNN architecture and updated model file

--- digits.py
+++ digits.py
@@ -8,21 +8,24 @@ import pygame
 import numpy as np
 import cv2
 
-class ANN(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(ANN, self).__init__()
-        self.fc1 = nn.Linear(28*28, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 128)
-        self.fc4 = nn.Linear(128, 10)
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(64*7*7, 128)
+        self.fc2 = nn.Linear(128, 10)
     
     def forward(self, x):
-        x = x.view(-1, 28*28)
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
         x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.fc2(x)
         return x
     
 # Load MNIST dataset
@@ -40,10 +43,10 @@ testloader = DataLoader(testset, batch_size=64, shuffle=False)
 
 import os
 
-model = ANN()
+model = CNN()
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-model_path = 'mnist_ann.pth'
+model_path = 'cnn_mnist.pth'
 
 def train_model(epochs):
     """Train the model for the specified number of epochs"""
